8e55280ff9be1355f684e0c566306e6322ea1b9f
ForgeWorks <forgeworks@proton.me>
2026-02-05 23:16:54 -0300

    Upload project files

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Olá! Bom dia a todos!";
+    cin.get();
+    cout << "Já dispenso introduções e vou direto ao ponto: Não sobrou tempo pra eu fazer meu portfólio e eu estou exhausto desse mês terrível.\nSão 2:34 da manhã de quarta e eu sinceramente desisto de fazer projetos de artes aceitáveis daqui em diante...";
+    cin.get();
+    cout << "Escrevi essa... coisa.. -_-; em C++ com um computador com 38% de bateria sem acesso a energia elétrica graças a minha 'querida' mãe <//3 -_-;";
+    cin.get();
+    cout << "Bora lá:\n";
+    cin.get();
+    system("clear");
+    cout << "Neste bimestre basicamente tivemos umas 3 aulas reais, e foi tudo projeto em grupo, que são uma dor imensa, muito obrigado prof <//3";
+    cin.get();
+    cout << "Foi um bimestre dedicado ao estudo da cinematografia e do 'Movimento do cinema novo'";
+    cin.get();
+    cout << "A primeira aula se tratou da introdução do movimento mencionado anteriormente por meio do filme Barra Vento (De Glauber Rocha) ";
+    cin.get();
+    cout << "A segunda aula se tratou da atribuição de um projeto EM GRUPO (HUDHAIUYAIHJDWAUUHJ) onde deveriamos retratar nossa vida no IFG por meio de fotos.";
+    cin.get();
+    cout << "Só vo passar um frame pq passar pra ascii é uma dor :3";
+    cin.get();
+    cout << R"(.,,;;;;;;;;;;;;;;::::::::::::::::,',,,''''''''''''.......'''''''',,,,,;;;;;:::::cccccclllllllllllllllllc.;:ccc
+    .;;;;;;;;;;;:::::::::::::::::::::,..'',,,,,;;;;;:::::ccccccclllllllllllllooooooooooooooooooodddooooooolc.:cccc
+    .;;;;;;;;;;;::::::::::::::::::::::;':cllllllllllllllloooooooooooooodddxxxxxxkkkkkOOOOO00000000koooooool:'ccccc
+    .;;;:;,,;;;::cc::::::::::::::::::::':llllllooddxxxxxxkkkkOOOO0000KKKKKKKKK0xxlxOxookxx00K00000koooooooo;'ccccc
+    .;;;::,;;;;::ccccc:::::::::::::::::,:llooooox0000000000000KK0KKKKKKKKKKKK0kc:loolcloccO0000000kddddoooo;,ccccc
+    .,,;;:;;;;;::cllll:::::::::::::c:::,;ooooooox0000KK0000000K00KKKKKKKKKKKK0kkkkO0kkO0OOO0000000kddddoool,;ccccc
+    .;;,,,,;;;::cclollc:::::::::::c::::;;oooooodx0000K0KKKKKKK00KK0K0K0KKKKKK0klclxdllldlollxlllxo;:c::;:;;'';,ccc
+    .;;;;;;;;:::ccooodccc:::cccccccccc:;,oooooddd000KKKKKKKKKKKKKKKK0KKKKKKKKK0xocoddooxooodxxxkkdclocccl::.;;;ccc
+    .;;;;;;;;:::clooxxccc::cccccccccccc:,oooodddd0KKKKKKKKKKKKKKKKKKKKKKKKKKKKKOkxoxxdloolxxkdkx0dxddddoooc.cccccc
+    .::::::;;,:::ccllccc:::cc:::ccccccc:'oooodddd0KKKKKKKKKKKKKKKKKKKKKKKKKKKKxl,:c:,l;;:o;:olccklddddooc;,.'';ccc
+    .;,'';,''''';';,';;,';:,,,'',;,;,';,.;cldddddO0000000000000000KKKKKKKKKKKKOkxxdkxkxxxkxdxkOOkOdooc,........,cc
+    .,',,:,,,;,,;;;;,;;;,::,,;;;;;;;;;;;.:cldddddk0kkkkkxxxkkkkkkO00000000000OOOOOOOOkkkkkkkkkkkkkoo;..........;cc
+    .:;::::;;;;::,:::::::::::cc:ccccccc:,ldddddddk0OkkkxxxxxOOOOOOOOOOOOkkkOOOkkkkkkxkkxkkkkkkkkkxl:'........;:ccc
+    .,''.,'..,.;..'.''.',..,.''',,'cccc:,cdddddddk0OkkxxxxxxOOkkOOOOOOOOOOOOOkkkxkkkkkkkkkkkkkkkkxl,........,ccccc
+    .:,;;,:;,;;;,,;,;;;;,::c;::;:;:ccccc;:dddddddx0OxxxxxxxxxxxxOOOOOOOOOOOOOkdllldkkkkkkkkkkkkxxxo:'......,:ccccc
+    .;;;;::;;;;;;;::::::::::cccccccccccc;;dddddddx0OkkkOOOOOOOOOOOOOOOOOOOOkkkl,''',::cccodxxxxxxdlll,..'.',;:cccc
+    .;:;;'.....';;;;:::::::::::ccccccccc;,dddddddd00000000000000OOOOOOOOOOOOOkdc,''.......'',,,,'''''...'''...';:c
+    .:;;........';;;;;:::::::::::cccccc:;'ooodddddO00000000000000000000000OOOOOkoc;'.............................'
+    .;;,.........,;;;;;;;;::::::::::::::;'ooooddddkkOOkkkkkOOOOOOkkkkkkkkkkkkxxdllll:;,...........................
+    .;;;,..........,,;;;;;;;;:::::::::::;.looooodddddddddddddddddddddddooooollllllllllcc:;'.......................
+    .,... .............';;;;;;;;::::::::;'coooooooodddddddddddddoooooooooollllllllllllccccc:;.....................
+    ....    ....   .......,;::::cccccccc:';:::ccccllllloooooooooooooooooollllllllllllcccccc::'....................
+    .... ..    .....;:::cccccccc;,'''','''''''''''''''''''''''',,,,,,,,,,,,,,,,,,,,,.................',;c
+    ....         ....;:::cccccccccccllllllllllllllccccccccc::;;;;;;;;;,,,,,,''''''..................:llll
+    ....   ....          ....;;::ccccccccllllllooooooooooooooooooolllllllllcccccccccccccccc'................llloo
+    .,,'.    ....          ..,;;;:::ccccclllllllooooooooooooooooooooollllllllllllllllllllllc.................;looo
+    .,,,......''''.....   ..',;;;::::ccccclllllllloooooooooooooooooolllllllllllllllllllllll,.................;looo
+    .''''....,;;,'....''...',,;::::::cccccclllllllllllllloooooooooollllllllllccccclllllllll'................,llllo
+    .,,,,,,'.........',,'..',,;;;:::::ccccccclllllllllllllllllllllllllllllllcccccccclllllcccc,.............,clllll
+    .c:;'.....    ....',,..',,,;;;;:::cccccccccllllllllllllllllllllllllllllccccccccccccccccccc,..........'clllllll
+    .'.. ..         ....''''',,,;;;;:::cccccccccccclllllllllllllllllllllllcccccccccccccccccccc,.    .....cllllllll
+    )";
+    cin.get();
+    cout << "E sobre o filme de artes, eu arruinei ele todo ao assumir a direção e animação. Minha má gerência nos impediu de terminar o filme. Segue um frame em ASCII";
+    cin.get();
+    cout << R"(.'ccccc::;,,,';cccccc:::::::::::::::cccccccccllllllllllloooooooooooooooooooooooooolllllcccccccccccccccccccc:::::::::::::::::::::::
+    .,;;,,''......,::::::ccccccccccccccclllllllllllllllllccclllllllccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    .............;ccccccccccccccccccccccllllllllllllllllllllllllllllllllllclllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+    ...........'cllllllllllllllloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+    .........;looooddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    oolc:::lodddddddddddddddddddddddddddddddddddddoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+    oooooooooooooooooodddddddddddddddddddddddddddoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolllllllllllllllllllllll
+    lllllllllooooooooooooooooooooddddddddddddddddddooooooooooooooooooooooooooooooooooooooolllllllllllooooooooollllllllllllllllllllllll
+    ::::cccccllllllllllllooooooooooooooooooooodddddddddddddddooooooooooooooooooooooooooooollllllllllllllllllllcccccccccccccccccccccccc
+    cccc:::::::::::ccccccccllllllllllllloooooooooooooooooooooooddddddddddooooooooooooooooollllllllllllllllllllllllllllllllllllllllllll
+    llllccccccccccc::::::::::::cccccccccccccclllll:,llllloooooooooooolcloooooooooooooooooolllllllllllllllllllloooooooooooooooooollllll
+    xddddoooollllllccccccccccccccccccc:::::::::::; .,:::ccccccccc:''...;::clllllllllooooollllllooooooooooooooooooooooooooooooooooooooo
+    xxxxxxxxxxxxxddddooooooolllllllc::ccclccccccc;  .:ccccccccccc,........':lllllllllllllccccccccccccccccccclllllloooooooooooooooooooo
+    ooooodddxxxxxxxxxxxxxxxxxxxl;;,. ..,;ldddoooo;.  ;ooollloddd:'..'.......:llllllllllllc:::::::::::::cccccccllllllllllllllllllllllll
+    ccccclllllloooddddddxxxxxxo,..      .';dxxxxx'  .:xxxxxxddd:.......',,,,cllllllllooooc:::::::::::::::::::cccccccccccccccccccllllll
+    :::::::ccccccclllllllolooo;. .      ...'cdxxx:..'lddddxxdll:::;,,.''',';cccccccclllllc::::::::::::::::::::cccccccccccccccccccccccc
+    ccccc:::::::::::ccccccccc,.           .':loooo:.'lloooooollllllc:,.;.....:ccccccccccc:,...;:::::::::::::::::::::cccccccccccccccccc
+    lccccccccc::::::::::::::,'''...   ....',ccllllc;:clllllllcccccccc,  ......:cccccccccc;....,ccccccc::::::::::::::::::::::::::::::::
+    llllllcccccccccc::::::::::::;,'...''';;:cc::c::;:::;;:;..,'';;;;;'.......';;;;;;:::::;,,;:cccccccccccccccc::::::::::::::::::::::::
+    oollllllllllcccccc:::;;;,,,,,'''..'''''''............... ..'...........................''',,,,;;;;;:::cccc::::::::::::::::::::::::
+    oodddooooolc:;,''''.'.'...............................................................................''',,,;;::::::::::::::::::::
+    cccclllllo,.....'..............................................................................................';c::::::::::::::::
+    cccc::cccc;.....................................................................................................,ccccccccccccccccc
+    lllllllllcc:'..................................................................................................':ccccccccccccccccc
+    oooooooooooloc,.............................................................................................';cllllllccccccccccccc
+    ooooooooooooooooc,........................................................................        .......,:lllllllllllllllllllllll
+    ooooooooooooddddoooc,.....................................................                       .....':ccllllllllllllllllllllllll
+    oooooooooooodddddddddo:........................................... .                  .',,......',;::clooloollllllllllllllllllllll
+    llooooooooooodddddddddddc'......................................                    .';;:::;;:ccccccccclooooooooooooolllllllllllll
+    lllloooooooooddddddddddddoc'.................  ............ .                      .,;;;::::c:ccccc::::looooooooooooooooooooooooll
+    ccccllloooooooddddddddddddool:;'...............                                     .';:::::cccc:;:::::clooooooooooooooooooooooooo
+    ::ccclllloooooooooddddddddoooooooc:,'............                   .';'.........    .'::::::ccc::::::cccllllllllloooooooooooooooo
+    :::::cclllloooooooooddddddooooooooooooc:,..........               .';;:::,'......  'clllc:::cccccc:::::::ccllllllllllllllllooooooo
+    ccc:::::ccllllooooooddddddoooooooooooooooolc:;,......             ,;:::::'....'.    'clllc:::cccccc:cclcccccllllllllllllllllllllll
+    lcccc:::::cccllooooooooodddoooooooooooooooooooc::,'....           ,:::::'.....'.      ..'..:ccccccccccccccccccllllllllllllllllllll
+    llccccc::::::cclllllooooooooooooooooooooooooooolccc:, ....        ;::c::.........          :ccccccccccc:::clccccccccclllllllllllll
+    oollllccccc::::ccclllooooooooooooooooooooooooooolcccc,          ..::ccc:,'...... ....      :ccccccc::::::;ccccccccccccccccccclllll
+    xxdollllcccc::::cccccllloooooooooooooooooooooooooccccc:.        .:c:cccc:'....     ........:ccccccc:;;;;;:cccccccccccccccccccccccc
+    xxxxxddllllccccc::::ccllllloooooooooooooooooooooolcccccc:,....';ccccccc::'...        ..'',;ccccccc:;;;::::;cc::ccccccccccccccccccc
+    kkkxxxxdddooccccc:::::cccclllooooooooooooooooooooollccccccccc:cccccccc;,,',,'.         ..;cccccccccc::;;;;:ccc:::::::::ccccccccccc
+    xxxkkkkxxdddllllccccc:::::cclllllloooooooooooooooooollccccccccccccccc:',,;;;:,..      ..;ccccccccccc;;;::ccccc::::::::::::::::cccc
+    xxxxxxxkkkkxdoolllccccc:::::cccllllloooooooooooooooolllccccccccc::;cclcc:::::::::;;;;:ccccccccccc:::;:cccccccc::::::::::::::::::::
+    loddxxxxxxkkkxxxdollllccc::::ccccccllooooooooooooooolllllllllccc;:ccllllccc:ccc:c:ccccccccccccc:;;;::::::::::c::::::::::::::::::::
+    llllloodxxxxxxxxxxxdolllcccc:::::cccllllloooooooooooolllllllllllllllllllllccccccccccccccccccccc;::::::::::::::::::::::::::::::::::
+    cccclllllodxxxxxxxxxxxddllccccc::::::cllllllloooooooolllllllllllccccllclllcccc:cccccccccccc:ccc:::::::::::::::::::::::::::::::::::
+    cc::cccclllllodxxxxxxxxxdooollcccc:::::ccclllooooooooollllllllllccccccccllllcccccccccccc:::c::::::::::::::::::::::::::::::::::::::
+    cc:::::::cclllllodxxkkkxxddoooolcccccccc:::ccllooooooolllllllllllccccccccccccccccccccccccccc::::::::::::::::::::::::::::::::::::::
+    ::::::::::::cccllllodxxkkkkxddoolllccccccccc:ccllllooolllllllllllccccccccccccccccccccccccccc::::::::::::::::::::::::::::::::::::::
+    cc:::::::::::::ccclloooddxkkkkkxolllllccccccccccccclllllllllllllllcccccccccccc:::::cccccccccc:::::::::::::::::::::::::::::::::::::
+    cccccc::::::::::cccccloooodxxkkkkkxdollllccccc:::cccccclllllllllllccccccccccccc:::::::::ccccc:::::::::::::::::::::::::::::::::::::
+    cccccccc::::::::::cccccclooddddxxkkkkxdolllllcc:::::cccclllllllllllcccccccccccc:::::::::::::c:::::::::::::::::::::::::::::::::::::
+    llccccccccc:::::::::cccccccodddddxkkkkkkxollllllc::::::clllllllllllcccccccccccc:::::::::::::::::::::::::::::::::::::::::::::::::::
+    )";
+    cin.get();
+    system("clear");
+    cout << "Eu não tenho aula predileta pois este bimestre foi uma dor e vergonha tremendas.";
+    cin.get();
+    system("clear");
+    cout << "Pronto, fim! Obrigado pelo seu tempo.";
+    cin.get();
+    while(true) {
+        std::cout << '\a' << std::flush;
+    }
+    return 0;
+}
